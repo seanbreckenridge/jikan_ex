a562fce2a9d1954265b63d0aba57e338a9a0453d
Sean Breckenridge <seanbrecke@gmail.com>
2020-05-29 14:06:59 -0700

    bump version to update documentation

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule JikanEx.MixProject do
   def project do
     [
       app: :jikan_ex,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
